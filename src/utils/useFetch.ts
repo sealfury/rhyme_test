907538b2868f5b5e403b33930d2e6b5853722cc7
sealfury <willspecker@gmail.com>
2022-03-16 13:44:33 +0100

    scaffolds fetch function

---
+++ src/utils/useFetch.ts
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react'
+
+export const useFetch = () => {
+  const API_URI = process.env.REACT_APP_API_URI
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState({ show: false, msg: '' })
+  const [data, setData] = useState(null)
+
+  const fetchArticles = async (url: string) => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+
+      if (!data) {
+        setError({ show: true, msg: data.error })
+      }
+
+      setData(data)
+      setIsLoading(false)
+    } catch (err) {
+      console.error(err)
+    }
+
+    useEffect(() => {
+      fetchArticles(API_URI as string)
+    }, [])
+
+    return { isLoading, error, data }
+  }
+}
